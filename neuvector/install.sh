73e1f4617e0fe126f18f510e6c5c959393e976e2
Colin Griffin <colin@krum.io>
2024-03-08 10:48:52 -0500

    update helm command

--- neuvector/install.sh
+++ neuvector/install.sh
@@ -2,4 +2,5 @@
 
 helm repo add neuvector https://neuvector.github.io/neuvector-helm/
 helm repo update
-helm install neuvector --namespace neuvector --create-namespace neuvector/core  --version 2.7.2 --set k3s.enabled=true
+
+helm upgrade --install neuvector --namespace neuvector --create-namespace neuvector/core  --version 2.7.2 --set k3s.enabled=true
